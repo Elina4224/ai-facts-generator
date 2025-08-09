618df5956f691d51a651867477657b8cd50079ca
Elina4224 <elina.huseynova.codes@gmail.com>
2025-08-08 22:36:57 -0500

    Added AI API integration

--- SRC/index.js
+++ SRC/index.js
@@ -1,12 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: ["Roses are red, violets are blue"],
+    strings: response.data.answer,
+
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "4t7238554e2bo320fcfcf12415f753fa";
+  let context =
+    "You a romantic poem expert and love to write short poems. Your mission is to generate a 4 liner poem in basic HTML. Sign the poem with bold SheCodes AI after the poem. Make sure to follow the user instructions.";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
