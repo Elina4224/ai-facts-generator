e7333c8d361e6c6aed3788463a5b554613a68b2e
Elina4224 <elina.huseynova.codes@gmail.com>
2025-08-10 11:00:13 -0500

    Updated CSS, made changes in a format of generator

--- SRC/index.js
+++ SRC/index.js
@@ -1,34 +1,30 @@
-function displayPoem(response) {
-  let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = ""; // clear old text
+function displayFact(response) {
+  let cleanText = response.data.answer.replace(/```html|```/g, "");
 
-  let cleanedText = response.data.answer
-    .replace(/```html/g, "")
-    .replace(/```/g, "");
-
-  new Typewriter("#poem", {
-    strings: cleanedText,
+  new Typewriter("#fact", {
+    strings: cleanText,
     autoStart: true,
+    loop: false,
     delay: 1,
     cursor: "",
   });
 }
 
-function generatePoem(event) {
+function generateFact(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "4t7238554e2bo320fcfcf12415f753fa";
   let context =
-    "You a romantic poem expert and love to write short poems. Your mission is to generate a 4 liner poem in basic HTML. Sign the poem with bold SheCodes AI after the poem. Make sure to follow the user instructions.";
-  let prompt = `Generate a poem about ${instructionsInput.value}`;
+    "You are a travel expert and know a lot of interesting and funny facts about different cities. Your mission is to generate an interesting/funny fact about the city in basic HTML. Sign the fact with bold SheCodes AI below it. Make sure to follow the user instructions.";
+  let prompt = `Generate a fact about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  let poemElement = document.querySelector("#poem");
-  poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="blink">Generating a poem about ${instructionsInput.value}</div>`;
-  axios.get(apiURL).then(displayPoem);
+  let factElement = document.querySelector("#fact");
+  factElement.classList.remove("hidden");
+  factElement.innerHTML = `<div class="blink">Generating a fact about ${instructionsInput.value}</div>`;
+  axios.get(apiURL).then(displayFact);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+let factFormElement = document.querySelector("#fact-generator-form");
+factFormElement.addEventListener("submit", generateFact);
